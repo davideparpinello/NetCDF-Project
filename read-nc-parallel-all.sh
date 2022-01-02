05b2d383b99c1a4260bd6a89872f7684e0580ef5
Parpinello <davide.parpinello@hpc-head-n2.unitn.it>
2022-01-02 17:20:05 +0100

    loop sui file

---
+++ read-nc-parallel-all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#PBS -l select=5:ncpus=10:mem=4gb
+# set max execution time
+#PBS -l walltime=0:02:00
+# imposta la coda di esecuzione
+#PBS -q short_cpuQ
+module load mpich-3.2
+module load netcdf-4.7.0--gcc-9.1.0
+module load hdf5-1.10.5--gcc-9.1.0
+mpirun.actual -n 5 NetCDF-Project/read-nc-parallel-all    #1, 5, 25, 73, 125, 365, 1825, 9125  num proc possibili
